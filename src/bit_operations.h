b1f9ef003cc71539a05cd2020df8811e109e2337
MaciejDziuban <dziuban.maciej@gmail.com>
2023-11-06 13:11:30 +0100

    Format code

--- src/bit_operations.h
+++ src/bit_operations.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#include "src/types.h"
 #include "src/error.h"
+#include "src/types.h"
 
-template<u8 bitIndex>
+template <u8 bitIndex>
 constexpr void setBit(u8 &value, bool bit) {
     static_assert(bitIndex < 8);
     value |= bit << bitIndex;
     value &= ~((!bit) << bitIndex);
 }
 
-template<u8 bitIndex>
+template <u8 bitIndex>
 constexpr bool isBitSet(u8 value) {
     static_assert(bitIndex < 8);
     return static_cast<bool>(value & (1 << bitIndex));
@@ -23,4 +23,3 @@ constexpr bool isSignBitSet(u8 value) {
 constexpr bool isZeroBitSet(u8 value) {
     return isBitSet<0>(value);
 }
-
